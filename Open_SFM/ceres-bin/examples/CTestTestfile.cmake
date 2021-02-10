f674881b98b7b24263902515fbf767d100360f00
Ricardo <ricardorico0527@gmail.com>
2021-02-10 09:40:00 +0100

    opensfm removed

--- Open_SFM/ceres-bin/examples/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ric/Documents/ENSTA/3/ProjetTutore/ceres-solver-2.0.0/examples
-# Build directory: /home/ric/Documents/ENSTA/3/ProjetTutore/ceres-bin/examples
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("sampled_function")
-subdirs("slam")
